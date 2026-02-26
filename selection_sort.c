4edaa3c0e09eceb7879deec2b3abf05dcdfbccca
Bhumik-V <bhumikvalluripalli@gmail.com>
2026-02-26 19:25:24 +0530

    Create selection_sort.c

---
+++ selection_sort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main() {
+    int n, i, j, min_index, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(i = 0; i < n; i++) {
+        printf("Enter element: ");
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n-1; i++) {
+        min_index = i;
+        for(j = i+1; j < n; j++) {
+            if(arr[j] < arr[min_index])
+                min_index = j;
+        }
+
+        temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+
+    printf("Sorted array: ");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
